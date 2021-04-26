3abfce4f41cf6948aa4d415602a71cbc3f889d90
Vinicius Rosa <viniciushcrosa@gmail.com>
2021-04-26 13:50:57 -0300

    pagina randomCostumer feita

--- pages/api/costumer/index.js
+++ pages/api/costumer/index.js
@@ -1,9 +1,15 @@
 import db from '../../../config/database'
 
 export  default async function Costumer(req, res){
-  const { rows } = await db.query(`SELECT * FROM costumer WHERE "madeContact" = false and "contacts" is not null ORDER BY random() LIMIT 1`, [])
+  try{
+    const { rows } = await db.query(`SELECT * FROM costumer WHERE "madeContact" = false and "contacts" is not null ORDER BY random() LIMIT 1`, [])
   
-  console.log(rows)
+    console.log(rows)
   
-  return res.send(rows[0])
+    return res.send(rows)
+  }catch(err){
+    console.log(err)
+    return res.status(400).send(err)
+  }
+  
 }
